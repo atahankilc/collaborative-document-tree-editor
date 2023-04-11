bfa29c5e8091b110aceef7a532dc560a4a4fc70d
Aylin Topcu <topcu.aylinn@gmail.com>
2023-04-11 10:45:35 +0300

    implement initialization of element

--- ElementClass.py
+++ ElementClass.py
@@ -1,6 +1,17 @@
+import uuid
+from Enums import Occurs
+
+
 class Element:
     def __init__(self, name, doctree, id=None):
-        pass
+        self.name = name
+        self.doctree = doctree
+        self.id = uuid.uuid4() if id is None else id
+        self.template = self.doctree.templates[name]
+        self.children = []
+        self.attrs = {}
+        self.text = ""
+        self.initialize_with_template()
 
     def getXML(self):
         pass
@@ -19,3 +30,9 @@ class Element:
 
     def setAttr(self, attr, value):
         pass
+
+    def initialize_with_template(self):
+        for child in self.template.children:
+            child_template = self.doctree.templates[child]
+            if child_template.occurs == Occurs.ONE or child_template.occurs == Occurs.ONE_MORE:
+                self.insertChild(Element(child, self.doctree), len(self.children))
