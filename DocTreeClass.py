f82d099c6f76992c37cdb4b104167265a0c20673
Aylin Topcu <topcu.aylinn@gmail.com>
2023-04-16 19:40:12 +0300

    add comments for doctree and element classes

--- DocTreeClass.py
+++ DocTreeClass.py
@@ -6,6 +6,17 @@ from Enums import Occurs
 class DocTree:
     # importfile will be implemented in Phase-2
     def __init__(self, templfile, importfile=None):
+        """
+        Constructor for the DocTree class
+
+        @param templfile: (dict) template of the document tree
+        @param importfile: the file to be imported into the document tree
+
+        @note The constructor initializes the document tree with the root element. Since Element class constructor
+        initializes the element based on the template by inserting all required children, the document tree
+        will have all the required elements.
+        """
+
         self.name = "Unnamed"
         self.templates = DocTree.generate_templates(templfile["elements"])
         self.users = {}
@@ -13,13 +24,32 @@ class DocTree:
         self.root = Element(templfile["root"], self, 0)
 
     def setName(self, name):
+        """
+        Sets the name of the document tree
+        @param name: (str) the name of the document tree
+        """
+
         self.name = name
 
     def getElementById(self, id):
+        """
+        Returns the element with the given ID
+
+        @param id: (int) the ID of the searched element
+        @return: searched element if found, None otherwise
+        """
+
         self.search_element(id=id)
         return self.searched_element
 
     def getElementByPath(self, path):
+        """
+        Returns the element with the given path
+
+        @param path: (str) the path of the searched element
+        @return: XML representation of the searched element if found
+        """
+
         path_array = path.split("/")
         path_array.reverse()
         self.search_element(path_array=path_array)
@@ -27,6 +57,12 @@ class DocTree:
             return self.searched_element.getXML()
 
     def deleteElement(self, id):
+        """
+        Deletes the element with the given ID
+
+        @param id: (int) the ID of the element to be deleted
+        """
+
         self.search_element(id=id)
         if self.searched_element is not None:
             for index, child in enumerate(self.searched_element.parent.children):
@@ -34,21 +70,65 @@ class DocTree:
                     self.searched_element.parent.removeChild(index)
 
     def attach(self, user, callback):
+        """
+        Attaches a user to the document tree
+
+        @param user: (User) the user to be attached
+        @param callback: (function) the callback function of the user to be called when the document tree is updated
+        """
+
         self.users[user] = callback
 
     def detach(self, user):
+        """
+        Detaches a user from the document tree
+
+        @param user: (User) the user to be detached
+        """
+
         self.users.pop(user)
 
     # export method will be implemented in Phase-2
     def export(self, exptype, filename):
         pass
 
-    def search_element(self, id = None, path_array=None):
+    def search_element(self, id=None, path_array=None):
+        """
+        Searches the element with the given ID or path
+
+        @param id: (int) ID of the element to be searched
+        @param path_array: (list) path of the element to be searched
+
+        @note The function searches the element with the given ID or path and sets the searched_element
+        attribute of the document tree to the found element.
+
+        @note Based on the type of the search, the function calls the corresponding search function (id_search or
+        path_search). These functions are given as parameters to the traverse function of the root element. The
+        traverse function traverses the document tree recursively and calls the given function on each element.
+        """
+
         def id_search(element):
+            """
+            Searches the element with the given ID
+
+            @param element: (Element) the element to be searched
+            """
+
             if element.id == id:
                 element.doctree.searched_element = element
 
         def path_search(element):
+            """
+            Searches the element with the given path
+            
+            @param element: (Element) the element to be searched
+
+            @note The function traverses the path array and checks if the element has the same name as the current
+            item in the path array. If the element has the same name, the function sets the current element to the
+            parent of the current element and continues the search. If the element does not have the same name,
+            the function breaks the loop and continues the search with the next element.
+            """
+
             current_element = element
             is_correct_path = True
             for item in path_array:
@@ -67,6 +147,13 @@ class DocTree:
 
     @staticmethod
     def generate_templates(elements):
+        """
+        Generates the template of the document tree as a dictionary
+
+        @param elements: (dict) the elements in the template of the document tree
+        @return: (dict) the templates of the document tree
+        """
+
         templates = {}
         for key in elements:
             templates[key] = DocTree.Template(elements[key]["attrs"],
@@ -78,12 +165,26 @@ class DocTree:
 
     class Template:
         def __init__(self, attrs, children, occurs):
+            """
+            Constructor for the Template class
+
+            @param attrs:
+            @param children:
+            @param occurs:
+            """
+
             self.attrs = attrs
             self.children = children
             self.hasTextualContent = True if 'text' in children else False
             self.occurs = Occurs.from_str(occurs)
 
         def change_children_arr_to_children_dict(self, templates):
+            """
+            Converts the children array to a dictionary
+
+            @param templates: (dict) the templates of the document tree
+            """
+
             children_dict = {}
             for child in self.children:
                 children_dict[child] = templates[child]
--- ElementClass.py
+++ ElementClass.py
@@ -5,6 +5,17 @@ from Decorators import update_users
 
 class Element:
     def __init__(self, name, doctree, id=None):
+        """
+        Constructor for the Element class
+
+        @param name: (str) the name of the element
+        @param doctree: (DocTree) the DocTree object that this element belongs to
+        @param id: (int) the unique ID of the element (If not provided, a new unique ID will be generated using uuid)
+
+        @note The constructor initializes the element with the template by inserting all necessary
+        children by calling the initialize_with_template method
+        """
+
         self.name = name
         self.doctree = doctree
         self.id = uuid.uuid4().int if id is None else id
@@ -17,6 +28,13 @@ class Element:
         self.initialize_with_template()
 
     def getXML(self, level=0):
+        """
+        Returns the XML representation of the element
+
+        @param level: (int) the level of the element in the tree (used for indentation)
+        @return: the XML representation of the element as a string
+        """
+
         xml = f"{'    ' * level}<{self.name} id='{self.id}'"
 
         for attr, value in self.attrs.items():
@@ -37,13 +55,39 @@ class Element:
         return xml
 
     def getText(self):
+        """
+        Checks if the element has textual content from the template and returns the text content
+
+        @return: the text content of the element as a string
+        """
+
         if self.template.hasTextualContent:
             return self.text
 
     @update_users(function_name="insertChild")
     def insertChild(self, element, pos):
+        """
+        Inserts a child element at the specified position.
+
+        @param element: (Element) the child element to insert
+        @param pos: (int) the position to insert the child element at
+
+        @note Before inserting the child element, the method checks if the child element is allowed to be inserted
+        by checking the template of the element. If the child element is allowed to be inserted,
+        the method inserts the child element at the given position and updates the users of the DocTree object.
+        If the child element is not allowed to be inserted, an exception is raised.
+
+        @note If the given position is not a valid position (a negative number), an exception is raised.
+
+        @note If the given position is greater than the number of children, the child element is inserted at the end.
+        """
+
+        if pos < 0:
+            raise Exception("Position is not valid")
+
         child_count = self.count_child_occurrence(element.name)
 
+        # check if the resulting element with the inserted child is valid according to the template
         if not self.check_child_occurrence(element.name, child_count + 1):
             raise Exception("Child element cannot be inserted")
 
@@ -52,12 +96,28 @@ class Element:
 
     @update_users(function_name="removeChild")
     def removeChild(self, pos):
-        if pos >= len(self.children):
-            raise Exception("Child element does not exist")
+        """
+        Removes the child element at the specified position.
+
+        @param pos: (int) the position of the child element to remove
+        @return: the removed element
+
+        @note Before removing the child element, the method checks if the child element is allowed to be removed
+        since the resulting element must still be valid. If the child element is allowed to be removed,
+        the method removes the child element at the given position and updates the users of the DocTree object.
+        If the child element is not allowed to be removed, an exception is raised.
+
+        @note If the given position is not a valid position (larger than number of children or negative number),
+        an exception is raised.
+        """
+
+        if pos >= len(self.children) or pos < 0:
+            raise Exception("Position is not valid")
 
         child = self.children[pos]
         child_count = self.count_child_occurrence(child.name)
 
+        # check if the resulting element with the removed child is valid according to the template
         if not self.check_child_occurrence(child.name, child_count - 1):
             raise Exception("Child element cannot be removed")
 
@@ -65,8 +125,27 @@ class Element:
 
     @update_users(function_name="updateChild")
     def updateChild(self, element, pos):
-        if pos >= len(self.children):
-            raise Exception("Child element does not exist")
+        """
+        Replaces the child element at the specified position with the given element.
+
+        @param element: (Element) the element to replace the child element with
+        @param pos: (int) the position of the child element to replace
+        @return: (Element) the replaced element
+
+        @note Before replacing the child element, the method checks if the child element is allowed to be replaced. If
+        the child to be removed has the same type as the child to be inserted, the method replaces the child element.
+        Otherwise, the method removes the child element at the given position and inserts the new child element at the
+        same position. The checks for this case are already implemented in the removeChild and insertChild methods.
+        If the remove operation is successful but the insert operation raises an exception, the method inserts the
+        removed child element back at the same position. If the child element is not allowed to be replaced,
+        an exception is raised.
+
+        @note If the given position is not a valid position (negative number or larger than number of children),
+        an exception is raised.
+        """
+
+        if pos >= len(self.children) or pos < 0:
+            raise Exception("Position is not valid")
 
         old_child = self.children[pos]
 
@@ -78,26 +157,59 @@ class Element:
             except Exception:
                 raise Exception("Child element cannot be updated")
             else:
-                self.insertChild(element, pos)
+                try:
+                    self.insertChild(element, pos)
+                except Exception:
+                    self.insertChild(old_child, pos)
+                    raise Exception("Child element cannot be updated")
 
         return old_child
 
     @update_users(function_name="setAttr")
     def setAttr(self, attr, value):
+        """
+        Sets the value of the attribute with the given name as a key value pair in the attrs dictionary.
+
+        @param attr: (str) the name of the attribute
+        @param value: the value of the attribute
+        """
+
         self.attrs[attr] = value
 
     def traverse(self, func):
+        """
+        Traverses the element and its children and applies the given function to each element. (pre-order traversal)
+
+        @param func: (function) the function to apply to each element
+        """
+
         func(self)
         for child in self.children:
             child.traverse(func)
 
     def initialize_with_template(self):
+        """
+        Initializes the element with the template. This method is called when the element is created.
+
+        @note To find the required children, the method checks the template of the element and inserts the children
+        with occurrence requirements of ONE or ONE_MORE.
+
+        @note Since this method is called upon creation of the element, the created elements are always valid.
+        """
+
         for child in self.template.children:
             child_template = self.doctree.templates[child]
             if child_template.occurs == Occurs.ONE or child_template.occurs == Occurs.ONE_MORE:
                 self.insertChild(Element(child, self.doctree), len(self.children))
 
     def count_child_occurrence(self, child_name):
+        """
+        Counts the number of children with the given name.
+
+        @param child_name: (str) the name of the child element to count
+        @return: (int) the number of children with the given name
+        """
+
         count = 0
         for child in self.children:
             if child.name == child_name:
@@ -105,6 +217,15 @@ class Element:
         return count
 
     def check_child_occurrence(self, child_name, child_count):
+        """
+        Checks if the given number of children with the given name is allowed based on the occurrence
+        requirements of the child element in the template.
+
+        @param child_name: (str) the name of the child element to check
+        @param child_count: (int) the number of children with the given name to check
+        @return: (bool) True if the given number of children with the given name is allowed, False otherwise
+        """
+
         if child_name not in self.template.children:
             return False
 
