be5ff86e70f46553e07e6828970a58e4c1476700
Aylin Topcu <topcu.aylinn@gmail.com>
2023-05-05 00:23:05 +0300

    create agent class

--- Server.py
+++ Server.py
@@ -2,6 +2,7 @@ import sys
 import socket
 import threading
 import UserClass
+from Enums import Status
 
 
 class Server:
@@ -16,10 +17,50 @@ class Server:
 
         while True:
             client, address = self.sock.accept()
-            # TODO: create agent
+            agent = Agent(client, address)
+            agent.start()
 
     def close(self):
         self.sock.close()
+
+
+class Agent(threading.Thread):
+    def __init__(self, client, address):
+        threading.Thread.__init__(self)
+        self.client = client
+        self.address = address
+        self.user = None
+
+    def run(self):
+        with self.client:
+            try:
+                while self.user is None or self.user.status == Status.UNAUTHORIZED:
+                    self.client.send("Enter username: ".encode())
+                    username = self.client.recv(1024).decode().strip()
+                    self.client.send("Enter email: ".encode())
+                    email = self.client.recv(1024).decode().strip()
+                    self.client.send("Enter fullname: ".encode())
+                    fullname = self.client.recv(1024).decode().strip()
+                    self.client.send("Enter password: ".encode())
+                    passwd = self.client.recv(1024).decode().strip()
+
+                    self.user = UserClass.User(username, email, fullname, passwd)
+                    self.user.auth(passwd)
+
+                    if self.user.status == Status.UNAUTHORIZED:
+                        self.client.send("Invalid username or password".encode())
+
+                while True:
+                    self.client.send("Enter command: ".encode())
+                    command = self.client.recv(1024).decode().strip()
+                    print(f"Received command: {command} from {self.user.username}")
+                    # TODO: make calls based on command
+                    if command == "close":
+                        break
+            except Exception as e:
+                print(f"Exception: {e}")
+            finally:
+                self.client.close()
 
 
 if __name__ == "__main__":
