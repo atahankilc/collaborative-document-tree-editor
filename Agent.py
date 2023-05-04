9d278b6df043528b548d4925e019d02d0cf65a85
Aylin Topcu <topcu.aylinn@gmail.com>
2023-05-05 00:33:39 +0300

    create separate file for agent class

---
+++ Agent.py
@@ -0,0 +1,58 @@
+import threading
+import UserClass
+from Enums import Status
+
+
+class Agent(threading.Thread):
+    def __init__(self, client, address):
+        threading.Thread.__init__(self)
+        self.client = client
+        self.address = address
+        self.user = None
+        self.request_handler_thread = None
+        self.notification_handler_thread = None
+
+    def run(self):
+        with self.client:
+            try:
+                while self.user is None or self.user.status == Status.UNAUTHORIZED:
+                    self.client.send("Enter username: ".encode())
+                    username = self.client.recv(1024).decode().strip()
+                    self.client.send("Enter email: ".encode())
+                    email = self.client.recv(1024).decode().strip()
+                    self.client.send("Enter fullname: ".encode())
+                    fullname = self.client.recv(1024).decode().strip()
+                    self.client.send("Enter password: ".encode())
+                    passwd = self.client.recv(1024).decode().strip()
+
+                    self.user = UserClass.User(username, email, fullname, passwd)
+                    self.user.auth(passwd)
+
+                    if self.user.status == Status.UNAUTHORIZED:
+                        self.client.send("Invalid username or password".encode())
+
+                self.request_handler_thread = threading.Thread(target=self.handle_requests)
+                self.request_handler_thread.start()
+
+                self.notification_handler_thread = threading.Thread(target=self.handle_notifications)
+                self.notification_handler_thread.start()
+
+                self.request_handler_thread.join()
+                self.notification_handler_thread.join()
+            except Exception as e:
+                print(f"Exception: {e}")
+            finally:
+                self.client.close()
+
+    def handle_requests(self):
+        while True:
+            self.client.send("Enter command: ".encode())
+            command = self.client.recv(1024).decode().strip()
+            print(f"Received command: {command} from {self.user.username}")
+            # TODO: make calls based on command
+            if command == "close":
+                break
+
+    # TODO
+    def handle_notifications(self):
+        pass
--- Server.py
+++ Server.py
@@ -1,8 +1,6 @@
 import sys
 import socket
-import threading
-import UserClass
-from Enums import Status
+import Agent
 
 
 class Server:
@@ -17,66 +15,11 @@ class Server:
 
         while True:
             client, address = self.sock.accept()
-            agent = Agent(client, address)
+            agent = Agent.Agent(client, address)
             agent.start()
 
     def close(self):
         self.sock.close()
-
-
-class Agent(threading.Thread):
-    def __init__(self, client, address):
-        threading.Thread.__init__(self)
-        self.client = client
-        self.address = address
-        self.user = None
-        self.request_handler_thread = None
-        self.notification_handler_thread = None
-
-    def run(self):
-        with self.client:
-            try:
-                while self.user is None or self.user.status == Status.UNAUTHORIZED:
-                    self.client.send("Enter username: ".encode())
-                    username = self.client.recv(1024).decode().strip()
-                    self.client.send("Enter email: ".encode())
-                    email = self.client.recv(1024).decode().strip()
-                    self.client.send("Enter fullname: ".encode())
-                    fullname = self.client.recv(1024).decode().strip()
-                    self.client.send("Enter password: ".encode())
-                    passwd = self.client.recv(1024).decode().strip()
-
-                    self.user = UserClass.User(username, email, fullname, passwd)
-                    self.user.auth(passwd)
-
-                    if self.user.status == Status.UNAUTHORIZED:
-                        self.client.send("Invalid username or password".encode())
-
-                self.request_handler_thread = threading.Thread(target=self.handle_requests)
-                self.request_handler_thread.start()
-
-                self.notification_handler_thread = threading.Thread(target=self.handle_notifications)
-                self.notification_handler_thread.start()
-
-                self.request_handler_thread.join()
-                self.notification_handler_thread.join()
-            except Exception as e:
-                print(f"Exception: {e}")
-            finally:
-                self.client.close()
-
-    def handle_requests(self):
-        while True:
-            self.client.send("Enter command: ".encode())
-            command = self.client.recv(1024).decode().strip()
-            print(f"Received command: {command} from {self.user.username}")
-            # TODO: make calls based on command
-            if command == "close":
-                break
-
-    # TODO
-    def handle_notifications(self):
-        pass
 
 
 if __name__ == "__main__":
