74540684eb55b05e3001fe75216fbc6422d21aa5
Aylin Topcu <topcu.aylinn@gmail.com>
2023-05-06 14:56:11 +0300

    create send and receive threads for client

--- Client.py
+++ Client.py
@@ -1,5 +1,7 @@
+import sys
 import socket
 import pickle
+import threading
 
 
 class Client:
@@ -9,9 +11,23 @@ class Client:
         self.port = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect(("localhost", self.port))
+        self.exit_flag = False
+        self.receive_thread = threading.Thread(target=self.receive_loop)
+        self.receive_thread.start()
+        self.send_thread = threading.Thread(target=self.send_loop)
+        self.send_thread.start()
 
     def send(self, data):
         self.sock.sendall(pickle.dumps(data))
+
+    def send_loop(self):
+        while True:
+            data = input()
+            self.send(data)
+
+            if data == "exit":
+                self.exit_flag = True
+                break
 
     def receive(self):
         serialized_data = b''
@@ -24,5 +40,23 @@ class Client:
                 break
         return pickle.loads(serialized_data)
 
+    def receive_loop(self):
+        while self.exit_flag is False:
+            data = self.receive()
+            print(data)
+
+        self.close()
+
     def close(self):
         self.sock.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1] == "--port":
+        try:
+            port = int(sys.argv[2])
+            client = Client(port)
+        except ValueError:
+            print("Invalid port number")
+    else:
+        print("usage: python Client.py --port <port_number>")
