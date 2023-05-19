ea8a89d63c988ba43e5e1dabcc005f7c2b440726
Atahan Kılıç <atahan.kilic@hotmail.com>
2023-05-19 14:52:25 +0300

    agent modified.

--- server/Agent.py
+++ server/Agent.py
@@ -28,54 +28,19 @@ class Agent(threading.Thread):
         with self.conn:
             try:
                 while self.user is None or self.user.status == Status.UNAUTHORIZED:
-                    self.conn.send(pickle.dumps("You need to register or login to use collaborative document tree "
-                                                "editor\nPlease write 'register' or 'login' to continue: "))
-                    auth_type = pickle.loads(self.receive()).strip()
+                    response = pickle.loads(self.receive()).strip().split()
+                    if response[0] == "login":
+                        self.login(response)
+                    elif response[0] == "signup":
+                        self.signup(response)
 
-                    if auth_type == "register":
-                        self.conn.send(pickle.dumps("Enter credentials: "))
-                        response = pickle.loads(self.receive()).strip()
-                        response = response.split()
-                        username = response[0]
-                        email = response[1]
-                        fullname = " ".join(response[2:-1])
-                        passwd = response[-1]
-
-                        if username in self.server.user_dict:
-                            self.conn.send(pickle.dumps("Username already exists. Please choose another username"))
-                            continue
-
-                        if not re.fullmatch(self.EMAIL_REGEX, email):
-                            self.conn.send(pickle.dumps("Please enter a valid email address"))
-                            continue
-
-                        self.user = User(username, email, fullname, passwd)
-                        self.user.auth(passwd)
-                        self.server.add_new_user(self.user, passwd)
-                    elif auth_type == "login":
-                        self.conn.send(pickle.dumps("Enter username and password: "))
-                        response = pickle.loads(self.receive()).strip()
-                        username, passwd = response.split()
-
-                        try:
-                            self.user = self.server.user_dict[username]
-                        except KeyError:
-                            self.conn.send(pickle.dumps("You are not registered. Please register first."))
-                            continue
-
-                        self.user.auth(passwd)
-                    else:
-                        self.conn.send(pickle.dumps("Invalid command. Please write 'register' or 'login' to continue"))
+                    if self.user is None:
                         continue
-
-                    if self.user.status == Status.UNAUTHORIZED:
+                    elif self.user.status == Status.UNAUTHORIZED:
                         self.conn.send(pickle.dumps("Invalid username or password"))
                     else:
                         token = self.user.login()
                         self.conn.send(pickle.dumps(f"token: {token}"))
-
-                # self.conn.send(pickle.dumps(f"Welcome, {self.user.username}. You are now logged in.\nYou can write "
-                #                             f"'help' to see the list of commands you can use.\n"))
 
                 self.request_handler_thread = threading.Thread(target=self.handle_requests)
                 self.request_handler_thread.start()
@@ -87,6 +52,27 @@ class Agent(threading.Thread):
                 self.notification_handler_thread.join()
             except Exception as e:
                 print(f"Exception: {e}")
+
+    def login(self, response):
+        username, passwd = response[1], response[2]
+
+        try:
+            self.user = self.server.user_dict[username]
+            self.user.auth(passwd)
+        except KeyError:
+            self.conn.send(pickle.dumps("You are not registered. Please register first."))
+
+    def signup(self, response):
+        username, email, fullname, passwd = response[1], response[2], " ".join(response[3:-1]), response[-1]
+
+        if username in self.server.user_dict:
+            self.conn.send(pickle.dumps("Username already exists. Please choose another username"))
+        elif not re.fullmatch(self.EMAIL_REGEX, email):
+            self.conn.send(pickle.dumps("Please enter a valid email address"))
+        else:
+            self.user = User(username, email, fullname, passwd)
+            self.user.auth(passwd)
+            self.server.add_new_user(self.user, passwd)
 
     def receive(self):
         serialized_data = b''
