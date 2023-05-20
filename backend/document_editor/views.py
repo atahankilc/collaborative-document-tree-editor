0f522772fb6e2f6da5658c1c58ab31f22b239945
Aylin Topcu <topcu.aylinn@gmail.com>
2023-05-20 19:06:07 +0300

    add open_document form name and refactor

--- backend/document_editor/views.py
+++ backend/document_editor/views.py
@@ -26,28 +26,25 @@ class Editor(View):
 
     @staticmethod
     def post(request):
+        client = ClientHandler.client_dict[request.session.session_key]
         if 'new_document' in request.POST:
             new_document = NewDocument(request.POST)
             if new_document.is_valid():
                 document_template = new_document.cleaned_data['new_document']
-                ClientHandler.client_dict[request.session.session_key].add_to_sending_queue(
-                    f"new_document {document_template}")
+                client.add_to_sending_queue(f"new_document {document_template}")
                 return render(request, 'document_editor/editor.html', {
-                    'server_response': ClientHandler.client_dict[
-                        request.session.session_key].pop_from_receiving_queue(),
+                    'server_response': client.pop_from_receiving_queue(),
                 })
         elif 'open_document' in request.POST:
             open_document = OpenDocument(request.POST)
             if open_document.is_valid():
                 document_id = open_document.cleaned_data['document_id']
-                ClientHandler.client_dict[request.session.session_key].add_to_sending_queue(
-                    f"open_document {document_id}")
+                client.add_to_sending_queue(f"open_document {document_id}")
                 return redirect('document', document_id=document_id)
         elif 'list_documents' in request.POST:
-            ClientHandler.client_dict[request.session.session_key].add_to_sending_queue("list_documents")
+            client.add_to_sending_queue("list_documents")
             return render(request, 'document_editor/editor.html', {
-                'server_response': ClientHandler.client_dict[
-                    request.session.session_key].pop_from_receiving_queue(),
+                'server_response': client.pop_from_receiving_queue(),
             })
         return redirect('editor')
 
